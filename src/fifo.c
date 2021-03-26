ff0fbc3bb8bfe0aa415b3e9a725905e9fa352c38
Lev Fomenko <flevvi@gmail.com>
2021-03-27 02:25:56 +0300

    fifo.c fixes. register cdev

--- src/fifo.c
+++ src/fifo.c
@@ -1,30 +1,23 @@
-#include <linux/init.h>  
 #include <linux/module.h>
-#include <linux/device.h>
 #include <linux/kernel.h>
-#include <linux/fs.h>    
+#include <linux/fs.h>
+#include <linux/cdev.h>
 
-#include "list.h"
+#include "string_buffer.h"
 
-#define  DEVICE_NAME "fifo"   
-#define  CLASS_NAME  "fif"    
+#define  DEVICE_NAME "fifo"
 
-MODULE_LICENSE("GPL");    
-MODULE_AUTHOR("Lev Fomenko"); 
-MODULE_DESCRIPTION("A simple FIFO device"); 
-MODULE_VERSION("0.1");           
-
-static int    majorNumber;              
-static struct class*  fifoClass  = NULL;
-static struct device* fifoDevice = NULL;
-
+static int g_major_number;
+struct cdev g_cdev;
+dev_t g_dev_num;
+//TODO +mutex  +slab_cache +string_buffer
 
 static int     dev_open(struct inode *, struct file *);
 static int     dev_release(struct inode *, struct file *);
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
 
-static struct file_operations fops =
+static struct file_operations g_fops =
 {
    .open = dev_open,
    .read = dev_read,
@@ -33,46 +26,49 @@ static struct file_operations fops =
 };
 
 static int __init fifo_init(void){
-   printk(KERN_INFO "FIFODev: init\n");
 
-   majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
-   if (majorNumber<0){
-      return majorNumber;
+   int ret = alloc_chrdev_region(&g_dev_num, 0, 1, DEVICE_NAME);
+   if (ret < 0) {
+      printk(KERN_ERR " FIFODev : alloc_chrdev_region failed\n");
+      return ret;
+   } else {
+      printk(KERN_INFO " FIFODev : alloc_chrdev_region successful\n");
    }
 
-   // Register the device class
-   fifoClass = class_create(THIS_MODULE, CLASS_NAME);
-   if (IS_ERR(fifoClass)){                
-      unregister_chrdev(majorNumber, DEVICE_NAME);
-      return PTR_ERR(fifoClass);          
+   g_major_number = MAJOR(g_dev_num);
+   printk(KERN_INFO "FIFODev : major number of our device is %d\n", g_major_number);
+   printk(KERN_INFO "FIFODev : to use mknod /dev/%s c %d 0\n", DEVICE_NAME, g_major_number);
+
+   cdev_init(&g_cdev, g_fops);
+   g_cdev.owner = THIS_MODULE
+
+   ret = cdev_add(g_cdev, g_dev_num, 1);
+   if (ret < 0) {
+           printk(KERN_ERR "FIFODev : device adding to the kernel failed\n");
+           return ret;
+   } else {
+           printk(KERN_INFO "FIFODev : device addition to the kernel successful\n");
    }
 
-   // Register the device driver
-   fifoDevice = device_create(fifoClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-   if (IS_ERR(fifoDevice)){               
-      class_destroy(fifoClass);           
-      unregister_chrdev(majorNumber, DEVICE_NAME);
-      printk(KERN_ALERT "Failed to create the device\n");
-      return PTR_ERR(fifoDevice);
-   }
    return 0;
 }
 
 static void __exit fifo_exit(void){
-   device_destroy(fifoClass, MKDEV(majorNumber, 0));     
-   class_unregister(fifoClass);                          
-   class_destroy(fifoClass);                             
-   unregister_chrdev(majorNumber, DEVICE_NAME);          
-   printk(KERN_INFO "FIFODev: fifo_exit\n");
+   cdev_del(g_cdev); /*removing the structure that we added previously*/
+   printk(KERN_INFO " FIFODev : removed the cdev from kernel\n");
+
+   unregister_chrdev_region(g_dev_num, 1);
+   printk(KERN_INFO  " FIFODev : unregistered the device numbers\n");
+   printk(KERN_ALERT " FIFODev : character driver is exiting\n");
 }
 
 static int dev_open(struct inode *inodep, struct file *filep){
-   
+
    return 0;
 }
 
-static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){   
-   return -EFAULT;              
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
+   return -EFAULT;
 }
 
 
@@ -83,6 +79,11 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
 static int dev_release(struct inode *inodep, struct file *filep){
    return 0;
 }
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lev Fomenko");
+MODULE_DESCRIPTION("A simple FIFO device");
+MODULE_VERSION("0.1");
 
 module_init(fifo_init);
 module_exit(fifo_exit);
